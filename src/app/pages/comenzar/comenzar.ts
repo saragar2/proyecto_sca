3c8a4aaa919f79a3ec863b2d04cfce0ef63e782f
Sara <garciamiguelsara@gmail.com>
2025-11-19 14:10:53 +0100

    hemos empezado a destroxzar todo en comenzar.ts

--- src/app/pages/comenzar/comenzar.ts
+++ src/app/pages/comenzar/comenzar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-comenzar',
@@ -9,5 +10,63 @@ import { RouterLink } from '@angular/router';
   styleUrl: './comenzar.css',
 })
 export class Comenzar {
+  usuario: String = '';
+  contraseña: String = '';
+  nombre: String = '';
+  edad: String = '';
+  grupoSang: String = '';
+  rinones: String = '';
+  estadoSalud: String = '';
+  religion: String = '';
 
+  private backendUrl = 'http://localhost:3000/api/users';
+
+  // Comenzar(usuario: String, contraseña: String, nombre: String, edad: String, grupoSang: String, rinones: String, estadoSalud: String, religion: String)
+  // {
+  //   this.usuario = usuario;
+  //   this.contraseña = contraseña;
+  //   this.nombre = nombre;
+  //   this.edad = edad;
+  //   this.grupoSang = grupoSang;
+  //   this.rinones = rinones;
+  //   this.estadoSalud = estadoSalud;
+  //   this.religion = religion;
+  // }
+
+  constructor(private http: HttpClient) {}
+
+  // crearUsuario(usuario: String, contraseña: String, nombre: String, edad: String, grupoSang: String, rinones: String, estadoSalud: String, religion: String): void
+  // {
+  //   const nuevoUsuario = this.Comenzar(this.usuario, contraseña, nombre, edad, grupoSang, rinones, estadoSalud, religion);
+  //   this.saveUser();
+  // }
+
+  guardarUsuario(): void
+  {
+    const nuevoUsuario: Comenzar =
+    {
+      usuario: this.usuario,
+      contraseña: this.contraseña,
+      nombre: this.nombre,
+      edad: this.edad,
+      grupoSang: this.grupoSang,
+      rinones: this.rinones,
+      estadoSalud: this.estadoSalud,
+      religion: this.religion
+    }
+
+    this.http.post(this.backendUrl, nuevoUsuario).subscribe({
+      next: (response) => {
+        console.log('Usuario creado exitosamente:', response);
+      },
+      error: (error) => {
+        console.error('Error al crear el usuario:', error);
+      }
+    })
+  }
+
+  saveUser(): void
+  {
+    localStorage.setItem('user', JSON.stringify(this));
+  }
 }
